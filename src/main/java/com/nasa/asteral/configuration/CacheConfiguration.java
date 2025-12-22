2b1a2d2fe2d65af562d7407b44d26916802d296d
Albon Idrizi <albonidrizi@gmail.com>
2025-12-22 14:03:24 +0100

    feat: enable Spring Cache with @EnableCaching
    - Create CacheConfiguration to activate caching framework
    - Prepares infrastructure for service-layer caching annotations

---
+++ src/main/java/com/nasa/asteral/configuration/CacheConfiguration.java
@@ -0,0 +1,9 @@
+package com.nasa.asteral.configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+}
